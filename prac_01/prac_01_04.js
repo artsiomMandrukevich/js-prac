4bdc6b5152cfdc63145ba37986ef7819325d35de
ArtemMandrukevich <fGUy7Zihrn2Q>
2024-01-26 17:49:22 +0100

    Prac 1. example 4.

--- prac_01/prac_01_04.js
+++ prac_01/prac_01_04.js
@@ -1,3 +1,7 @@
+/*
+Enter two digits. Print a digit between a and b if the binary sum of this digit is 4.
+*/ 
+
 let a = +prompt("Input a");
 let b = +prompt("Input b");
 
@@ -8,3 +12,15 @@ if (a > b) {
 if (a <=0) {
     a = 0;
 }
+
+for(let i=a; i <= b; i++) {
+    let testNumber = (i >>> 0).toString(2);
+    let sum = 0;
+    for (let i = 0; i < testNumber.length; i++) {
+        sum += Number(testNumber[i]);
+    }
+    if(sum === 4) {
+        alert(i + " " + testNumber);
+    }
+}
+
